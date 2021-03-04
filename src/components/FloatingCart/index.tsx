a9d7fe5641ac36c197631fcbd273b035009326c0
Duany Dreyton <duanyrf@gmail.com>
2021-03-04 00:35:14 -0300

    finish all tests succesfully

--- src/components/FloatingCart/index.tsx
+++ src/components/FloatingCart/index.tsx
@@ -24,15 +24,22 @@ const FloatingCart: React.FC = () => {
   const navigation = useNavigation();
 
   const cartTotal = useMemo(() => {
-    // TODO RETURN THE SUM OF THE PRICE FROM ALL ITEMS IN THE CART
+    const total = products.reduce((accumulator, prod) => {
+      const subtotal = prod.quantity * prod.price;
 
-    return formatValue(0);
+      return accumulator + subtotal;
+    }, 0);
+
+    return formatValue(total);
   }, [products]);
 
   const totalItensInCart = useMemo(() => {
-    // TODO RETURN THE SUM OF THE QUANTITY OF THE PRODUCTS IN THE CART
+    const total = products.reduce(
+      (accumulator, prod) => accumulator + prod.quantity,
+      0,
+    );
 
-    return 0;
+    return total;
   }, [products]);
 
   return (
